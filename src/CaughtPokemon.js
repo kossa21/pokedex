a80fa275251200bd7e19b41e62212ab7416c1f3f
Anandamaya <arnoananda@gmail.com>
2020-06-19 14:10:47 +0200

    class exercise d ready

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -3,7 +3,16 @@ import React from "react";
 const CaughtPokemon = props => {
   const [totalCaught, setTotalCaught] = React.useState(0);
 
-  return <p>Caught {totalCaught} Pokemon on {props.date}</p>
+  const increaseCaught = () => {
+    setTotalCaught(totalCaught + 1);
+  }
+
+  return (
+    <div class="caughtPokemon">
+      <p>Caught {totalCaught} Pokemon on {props.date}</p>
+      <button onClick={increaseCaught}>Catch a Pokemon</button>
+    </div>
+  );
 }
 
 export default CaughtPokemon;
